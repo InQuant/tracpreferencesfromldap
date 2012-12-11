1eb6fe1b48600f574b0d73e50037072a89c41476
rain0r <rainer@hihn.org>
2012-12-11 10:28:15 +0100

    first working version

--- tracpreferencesfromldap/ldap_pref.py
+++ tracpreferencesfromldap/ldap_pref.py
@@ -2,7 +2,7 @@
 #
 #  File Name: file.py
 #  Creation Date: 2012 Jul 24
-#  Last Modified: 2012 Dez 10
+#  Last Modified: 2012 Dez 11
 
 #  Copyright (c) 2003-2012 InQuant GmbH
 #
@@ -29,22 +29,92 @@ from trac.core import *
 from trac.util.html import html
 from trac.web import IRequestHandler
 from trac.web.chrome import INavigationContributor
+from genshi.builder import tag
+from trac.util.translation import _
+from trac.web.api import IAuthenticator
+from trac.web.auth import LoginModule
+from trac.db import with_transaction
+
+import wcldapadmin
 
 class LdapPrefPlugin(Component):
     implements(INavigationContributor, IRequestHandler)
+    cn  = 'Windchill'
+    o   = 'ptc'
+
+    def __init__(self):
+        pass
+
+    def get_ldap_data(self, authname):
+        ldapdata = {
+                'server_host_name'  : '192.168.123.11',
+                'server_port'       : 8099,
+                'manager'           : 'cn=manager',
+                'manager_pwd'       : 'secret',
+                'ldapadd_cmd'       : 'ldapadd -x -h %(host)s -p %(port)s -D "%(manager)s" -w %(manpwd)s',
+                'ldapmodify_cmd'    : 'ldapmodify -x -h %(host)s -p %(port)s -D "%(manager)s" -w %(manpwd)s',
+                'ldapsearch_cmd'    : 'ldapsearch -x -h %(host)s -p %(port)s -D "%(manager)s" -w %(manpwd)s',
+                'root_dn'           : 'o=ptc',
+                'ldapdelete_cmd'    : 'ldapdelete -x -h %(host)s -p %(port)s -D "%(manager)s" -w %(manpwd)s',
+                'ldappasswd_cmd'    : 'ldappasswd -x -h %(host)s -p %(port)s -D "%(manager)s" -w %(manpwd)s -s %(newpwd)s %(uid)s'
+                }
+        ldap= wcldapadmin.WcLdapCmd(ldapdata)
+        dn= 'uid=%s,cn=%s,o=%s' % (authname, self.cn, self.o)
+        return ldap.getUser(dn)
+
+    def update_settings(self, authname, field, value):
+        @with_transaction(self.env)
+        def _insertTestCase(db):
+            c = db.cursor()
+            sql = "INSERT INTO \"session_attribute\" VALUES('%s',1,'%s','%s');" % (authname, field, value)
+            c.execute(sql)
+
+    def is_update_needed(self, authname, field):
+        """Check if the setting is empty
+        """
+        stmt  = "SELECT value FROM session_attribute WHERE sid = '%s' and name = '%s' LIMIT 1;" % (authname, field)
+        dbs = []
+        @with_transaction(self.env)
+        def _getTestCases(db):
+            c= db.cursor()
+
+            c.execute(stmt)
+            dbs.append(c.fetchone())
+        ret = None
+        if len(dbs) < 1 or \
+                dbs.pop() == None:
+            #val = dbs.pop()[0]
+            ret = True # update is needed
+        else:
+            ret = False
+        return ret
 
     # INavigationContributor methods
     def get_active_navigation_item(self, req):
-        return 'helloworld'
+        return 'tracpreferencesfromldap'
+
     def get_navigation_items(self, req):
-        yield ('mainnav', 'helloworld',
-            html.A('Hello world', href= req.href.helloworld()))
+        pass
+        # yield ('mainnav', 'tracpreferencesfromldap',
+        #     html.A('Hello world', href= req.href.tracpreferencesfromldap()))
 
     # IRequestHandler methods
     def match_request(self, req):
-        return req.path_info == '/helloworld'
-    def process_request(self, req):
-        content = 'Hello World!'
-        req.send_response(200)
-        req.send_header('Content-Type', 'text/plain')
+        from ipdb import set_trace; set_trace() 
+        if req.authname != 'anonymous':
+            if self.is_update_needed(req.authname, 'name'):
+                data = self.get_ldap_data(req.authname)
+                try:
+                    self.update_settings(req.authname, 'name', data.get('cn'))
+                except:
+                    pass
+            if self.is_update_needed(req.authname, 'email'):
+                data = self.get_ldap_data(req.authname)
+                try:
+                    self.update_settings(req.authname, 'email', data.get('mail'))
+                except:
+                    pass
+        return req.path_info == '/login'
 
+    def process_request(self, req):
+        passs
