8c2ebbd7b7c70aef9ed7de0fd7bb999a08dcc97f
rain0r <rainer@hihn.org>
2012-12-20 13:36:27 +0100

    added description, changed to version 1.0

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '1.0'
 
 setup(name='tracpreferencesfromldap',
       version=version,
-      description="",
-      long_description="""\
+      description="Load 'Full Name' and 'E-Mail-Address' from LDAP",
+      long_description="""This Plugin connects to a LDAP-Server that is configured in the trac.ini. It looks up the Full Name and the E-Mail-Address of a user who is identified by the Trac-Login-Name.
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      keywords='trac, ldap',
       author='Rainer Hihn',
       author_email='rainer.hihn@inquant.de',
       url='www.inquant.de',
